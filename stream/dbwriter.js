03b17fecd12885ff7174c6c474523d1ef6fd49fd
Peter Johnson <insomnia@rcpt.at>
2016-09-19 14:32:20 +0200

    code comments

--- stream/dbwriter.js
+++ stream/dbwriter.js
@@ -1,18 +1,21 @@
 
-var through = require('through2');
+var through = require("through2");
 
 function streamFactory(db, done){
 
   // vanity statistics
   var total_saved = 0;
 
+  // sqlite3 prepared statements
   var stmt = {
     names: db.prepare("INSERT INTO street_names (rowid, id, name) VALUES (NULL, $id, $name);"),
     line: db.prepare("INSERT INTO street_polyline (id, line, minX, maxX, minY, maxY) VALUES ($id, $line, $minX, $maxX, $minY, $maxY);")
   };
 
+  // create a new stream
   return through.obj({ highWaterMark: 8 }, function( batch, _, next ){
 
+    // vanity statistics
     total_saved += batch.length;
     console.error( total_saved );
 
@@ -20,7 +23,8 @@ function streamFactory(db, done){
     db.serialize(function() {
 
       // start transaction
-      db.run("BEGIN", function(){
+      db.run("BEGIN", function(err){
+        onError("BEGIN")(err);
 
         // import batch
         batch.forEach( function( parsed ){
@@ -30,7 +34,7 @@ function streamFactory(db, done){
             stmt.names.run({
               $id:   parsed.id,
               $name: name
-            }, onError('names'));
+            }, onError("names"));
           });
 
           // insert line in to polyline table
@@ -41,26 +45,30 @@ function streamFactory(db, done){
             $maxX: parsed.maxX,
             $minY: parsed.minY,
             $maxY: parsed.maxY
-          }, onError('line'));
+          }, onError("line"));
         });
       });
 
       // commit transaction
       db.run("COMMIT", function(err){
-        onError('COMMIT')(err);
+        onError("COMMIT")(err);
 
         // wait for transaction to complete before continuing
         next();
       });
     });
 
-  }, function(){
+  }, function done(){
 
     // finalize prepared statements
-    stmt.names.finalize();
-    stmt.line.finalize();
+    stmt.names.finalize( onError("finalize names") );
+    stmt.line.finalize( onError("finalize line") );
 
-    // insert in to rtree
+    // copy bbox data from 'street_polyline' to 'street_rtree' in a single statement.
+    // this uses more disk (as the floats are stored twice) but presumably yeild better performance
+    // than building the rtree incrementally; because rtrees need to be rebalanced as they are built.
+    // note: after this operation it should be safe to drop those columns from 'street_polyline'.
+    // see: https://www.sqlite.org/faq.html#q11
     db.run("INSERT INTO street_rtree (id, minX, maxX, minY, maxY) SELECT id, minX, maxX, minY, maxY FROM street_polyline;", function(){
 
       // we are done
@@ -70,6 +78,7 @@ function streamFactory(db, done){
   });
 }
 
+// generic error handler
 function onError( title ){
   return function( err ){
     if( err ){
