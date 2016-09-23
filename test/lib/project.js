b9f648edec122c5d755af9261f0a202ff646f04b
missinglink <insomnia@rcpt.at>
2016-09-23 18:36:35 +0200

    add tests for project.pointOnLine

--- test/lib/project.js
+++ test/lib/project.js
@@ -26,6 +26,43 @@ module.exports.project.pointOnEdge = function(test) {
   });
 };
 
+module.exports.project.pointOnLine = function(test) {
+  test('pointOnLine: simple', function(t) {
+
+    var l = [
+          [ -10.0, 0.0 ],
+          [   0.0, 0.0 ],
+          [ +10.0, 0.0 ],
+        ],
+        p = [ 0, 10.0 ];
+
+    var res = project.pointOnLine( l, p );
+    t.deepEqual(res, {
+      dist: 10,
+      edge: [ [ -10, 0 ], [ 0, 0 ] ],
+      point: [ 0, 0 ]
+    });
+    t.end();
+  });
+  test('pointOnLine: complex', function(t) {
+
+    var l = [
+          [ -10.0, +5.5 ],
+          [   0.0,  0.0 ],
+          [ +10.0, -5.5 ],
+        ],
+        p = [ +5.5, -5.5 ];
+
+    var res = project.pointOnLine( l, p );
+    t.deepEqual(res, {
+      dist: 2.1686343739747014,
+      edge: [ [ 0, 0 ], [ 10, -5.5 ] ],
+      point: [ 6.54510556621881, -3.5998080614203456 ]
+    });
+    t.end();
+  });
+};
+
 module.exports.all = function (tape) {
 
   function test(name, testFunction) {
