2afb0f7199e11b536e341c6c0d79dcad65b9fb78
missinglink <insomnia@rcpt.at>
2016-09-23 05:19:48 +0200

    conflate.sh script

--- conflate.sh
+++ conflate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OAPATH='/data/oa';
+OAPATH='/data/oa_global';
 rm 'planet_conflate.out' 'planet_conflate.err' &>/dev/null;
 
 find $OAPATH -type f -iname "*.csv" -print0 | while IFS= read -r -d $'\0' filename; do
@@ -8,7 +8,7 @@ find $OAPATH -type f -iname "*.csv" -print0 | while IFS= read -r -d $'\0' filena
     echo "skip $filename";
   else
     echo "conflate $filename";
-    echo "$filename" >planet_conflate.out;
-    bash -c "(cat $filename | time -p node conflate_oa.js oa.db street.db >planet_conflate.out) &> planet_conflate.err";
+    echo "$filename" >>planet_conflate.out;
+    bash -c "(cat $filename | time -p node conflate_oa.js oa.db planet.db >>planet_conflate.out) &>> planet_conflate.err";
   fi;
 done;
