b75ca241584ad7b731cf35c7f458abe5f31efcf2
missinglink <insomnia@rcpt.at>
2016-09-24 15:58:47 +0200

    add functional tests: west 26th street

--- test/_func.js
+++ test/_func.js
@@ -4,7 +4,8 @@ var common = {};
 
 var tests = [
   require('./functional/basic/run.js'),
-  require('./functional/disjoined/run.js')
+  require('./functional/disjoined/run.js'),
+  require('./functional/west26th/run.js')
 ];
 
 tests.map(function(t) {
