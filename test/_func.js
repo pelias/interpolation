875571f44b055f233f574197720129810c277086
missinglink <insomnia@rcpt.at>
2016-09-24 16:10:56 +0200

    add functional tests: rigaer straße

--- test/_func.js
+++ test/_func.js
@@ -5,7 +5,8 @@ var common = {};
 var tests = [
   require('./functional/basic/run.js'),
   require('./functional/disjoined/run.js'),
-  require('./functional/west26th/run.js')
+  require('./functional/west26th/run.js'),
+  require('./functional/updown/run.js')
 ];
 
 tests.map(function(t) {
