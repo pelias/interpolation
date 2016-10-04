75b486801d2581a3f1df526d98eefbc74eb7ff15
missinglink <insomnia@rcpt.at>
2016-10-04 19:46:54 +0200

    add sqlite tmp dir to conflate runner

--- conflate.sh
+++ conflate.sh
@@ -8,6 +8,12 @@ STREET_DB='/data/planet.db';
 PROC_STDOUT='/data/conflate.out';
 PROC_STDERR='/data/conflate.err';
 
+# a directory with enough free space to store sqlite tmp files
+SQLITE_TMPDIR='/data/tmp';
+
+# ensure tmpdir exists
+[ -d $SQLITE_TMPDIR ] || mkdir $SQLITE_TMPDIR;
+
 # delete stdio files
 rm $PROC_STDOUT $PROC_STDERR &>/dev/null;
 
@@ -15,7 +21,7 @@ rm $PROC_STDOUT $PROC_STDERR &>/dev/null;
 bash -c "./concat_oa.sh | time -p node conflate_oa.js $ADDRESS_DB $STREET_DB 1>$PROC_STDOUT 2>$PROC_STDERR";
 
 # create indexes
-bash -c "time -p sqlite3 $ADDRESS_DB \"CREATE INDEX address_id_idx ON address(id);\"";
-bash -c "time -p sqlite3 $ADDRESS_DB \"CREATE INDEX address_source_idx ON address(source);\"";
-bash -c "time -p sqlite3 $ADDRESS_DB \"CREATE INDEX address_parity_idx ON address(parity);\"";
-bash -c "time -p sqlite3 $ADDRESS_DB \"CREATE INDEX address_housenumber_idx ON address(housenumber);\"";
+bash -c "SQLITE_TMPDIR=$SQLITE_TMPDIR time -p sqlite3 $ADDRESS_DB \"CREATE INDEX address_id_idx ON address(id);\"";
+bash -c "SQLITE_TMPDIR=$SQLITE_TMPDIR time -p sqlite3 $ADDRESS_DB \"CREATE INDEX address_source_idx ON address(source);\"";
+bash -c "SQLITE_TMPDIR=$SQLITE_TMPDIR time -p sqlite3 $ADDRESS_DB \"CREATE INDEX address_parity_idx ON address(parity);\"";
+bash -c "SQLITE_TMPDIR=$SQLITE_TMPDIR time -p sqlite3 $ADDRESS_DB \"CREATE INDEX address_housenumber_idx ON address(housenumber);\"";
