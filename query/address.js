e87309a8ecf2fb4fb96f05d875978a0df4fb1ea6
missinglink <insomnia@rcpt.at>
2016-10-04 19:14:15 +0200

    optimize address query

--- query/address.js
+++ query/address.js
@@ -9,20 +9,26 @@
     $name: "foo", // the name of the street ( normalized by libpostal first )
   }
 
+  SELECT address.* FROM street.rtree
+  JOIN street.names ON street.names.id = street.rtree.id
+  JOIN address ON address.id = street.rtree.id
+  WHERE (
+    street.rtree.minX<=174.766843 AND street.rtree.maxX>=174.766843 AND
+    street.rtree.minY<=-41.288788 AND street.rtree.maxY>=-41.288788
+  ) AND street.names.name = "glasgow street";
+
 **/
 
 module.exports = function( db, params, cb ){
 
   var sql = [
-    "SELECT address.* FROM address",
-    "JOIN street.rtree ON address.id = street.rtree.id",
+    "SELECT address.* FROM street.rtree",
     "JOIN street.names ON street.names.id = street.rtree.id",
-    "JOIN street.polyline ON street.polyline.id = street.rtree.id",
-    "WHERE (",
-      "street.rtree.minX<=$lon AND street.rtree.maxX>=$lon AND",
-      "street.rtree.minY<=$lat AND street.rtree.maxY>=$lat",
-    ")",
-    "AND street.names.name = $name",
+    "JOIN address ON address.id = street.rtree.id",
+    "WHERE ",
+    "street.rtree.minX<=$lon AND street.rtree.maxX>=$lon AND",
+    "street.rtree.minY<=$lat AND street.rtree.maxY>=$lat AND",
+    "street.names.name = $name",
     "ORDER BY address.housenumber ASC;"
   ].join(' ');
 
