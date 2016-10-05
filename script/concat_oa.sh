95836c3e2019a82c2f98c4d0f069371e505c1a7e
missinglink <insomnia@rcpt.at>
2016-10-05 17:29:25 +0200

    formatting

--- script/concat_oa.sh
+++ script/concat_oa.sh
@@ -5,11 +5,12 @@
 
 OAPATH='/data/oa';
 
-find $OAPATH -type f -iname "*.csv" ! -name '*summary*' -print0 | while IFS= read -r -d $'\0' filename; do
-  # concat output (removing duplicates)
-  # Use cat -n to prepend line numbers
-  # Use sort -u remove duplicate data
-  # Use sort -n to sort by prepended number
-  # Use cut to remove the line numbering
-  cat -n $filename | sort -uk2 | sort -nk1 | cut -f2-;
-done;
+find $OAPATH -type f -iname "*.csv" ! -name '*summary*' -print0 |\
+  while IFS= read -r -d $'\0' filename; do
+    # concat output (removing duplicates)
+    # Use cat -n to prepend line numbers
+    # Use sort -u remove duplicate data
+    # Use sort -n to sort by prepended number
+    # Use cut to remove the line numbering
+    cat -n $filename | sort -uk2 | sort -nk1 | cut -f2-;
+  done;
