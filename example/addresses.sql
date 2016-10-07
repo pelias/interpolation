634b1baf0b9089284a63d66b06a5d032cb130004
Peter Johnson <insomnia@rcpt.at>
2016-10-07 18:11:11 +0200

    remove example, duplicate of test/functional/basic

--- example/addresses.sql
+++
@@ -1,9 +0,0 @@
-SELECT address.* FROM address
-JOIN street.rtree ON address.id = street.rtree.id
-JOIN street.names ON street.names.id = street.rtree.id
-JOIN street.polyline ON street.polyline.id = street.rtree.id
-WHERE (
-  street.rtree.minX<=174.766843 AND street.rtree.maxX>=174.766843 AND
-  street.rtree.minY<=-41.288788 AND street.rtree.maxY>=-41.288788
-) AND street.names.name = "glasgow street"
-ORDER BY address.housenumber ASC;
--- example/rtree.sql
+++
@@ -1,7 +0,0 @@
-SELECT street.polyline.id, street.names.name, street.polyline.line FROM street.rtree
-JOIN street.names ON street.names.id = street.rtree.id
-JOIN street.polyline ON street.polyline.id = street.rtree.id
-WHERE (
-  street.rtree.minX<=174.766843 AND street.rtree.maxX>=174.766843 AND
-  street.rtree.minY<=-41.288788 AND street.rtree.maxY>=-41.288788
-) AND street.names.name = "glasgow street";
