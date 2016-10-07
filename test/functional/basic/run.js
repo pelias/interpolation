5aee643750fde33f7e5bcce13501287f744701d3
Peter Johnson <insomnia@rcpt.at>
2016-10-07 18:49:01 +0200

    test: basic: simplify fixtures

--- test/functional/basic/run.js
+++ test/functional/basic/run.js
@@ -42,15 +42,15 @@ module.exports.functional.street_counts = function(test) {
 
     // count polyline table
     var polylines = sqlite3.count( paths.db.street, 'polyline' );
-    t.equal(polylines, 144, 'count(polyline)');
+    t.equal(polylines, 1, 'count(polyline)');
 
     // count names table
     var names = sqlite3.count( paths.db.street, 'names' );
-    t.equal(names, 165, 'count(names)');
+    t.equal(names, 1, 'count(names)');
 
     // count rtree table
     var rtree = sqlite3.count( paths.db.street, 'rtree' );
-    t.equal(rtree, 144, 'count(rtree)');
+    t.equal(rtree, 1, 'count(rtree)');
 
     t.end();
   });
@@ -61,7 +61,7 @@ module.exports.functional.address_counts = function(test) {
 
     // count address table
     var addresses = sqlite3.count( paths.db.address, 'address' );
-    t.equal(addresses, 497, 'count(address)');
+    t.equal(addresses, 25, 'count(address)');
 
     t.end();
   });
@@ -71,15 +71,15 @@ module.exports.functional.spotcheck = function(test) {
   test('spot checks', function(t) {
 
     // counts for a specific street
-    var count1 = sqlite3.count( paths.db.address, 'address', 'WHERE id=137' );
+    var count1 = sqlite3.count( paths.db.address, 'address', 'WHERE id=1' );
     t.equal(count1, 25);
 
     // counts for a specific street (open addresses)
-    var count2 = sqlite3.count( paths.db.address, 'address', 'WHERE id=137 AND source="OA"' );
+    var count2 = sqlite3.count( paths.db.address, 'address', 'WHERE id=1 AND source="OA"' );
     t.equal(count2, 11);
 
     // counts for a specific street (vertexes)
-    var count3 = sqlite3.count( paths.db.address, 'address', 'WHERE id=137 AND source="VERTEX"' );
+    var count3 = sqlite3.count( paths.db.address, 'address', 'WHERE id=1 AND source="VERTEX"' );
     t.equal(count3, 14);
 
     t.end();
@@ -90,33 +90,33 @@ module.exports.functional.end_to_end = function(test) {
   test('end to end', function(t) {
 
     // full interpolation for a single street
-    var rows = sqlite3.exec( paths.db.address, 'SELECT * FROM address WHERE id=137 ORDER BY housenumber' );
+    var rows = sqlite3.exec( paths.db.address, 'SELECT * FROM address WHERE id=1 ORDER BY housenumber' );
     t.deepEqual(rows, [
-      '80|137|OA|1.0|-41.2871999|174.766753|R|-41.2873014|174.7666946',
-      '100|137|VERTEX|2.373||||-41.287388|174.766845',
-      '99|137|VERTEX|3.207||||-41.287461|174.766921',
-      '98|137|VERTEX|3.959||||-41.287533|174.766983',
-      '97|137|VERTEX|4.637||||-41.287606|174.767028',
-      '85|137|OA|7.0|-41.2877481|174.7674712|R|-41.2878827|174.7671406',
-      '86|137|OA|9.0|-41.2878189|174.7673061|R|-41.2878858|174.7671419',
-      '96|137|VERTEX|9.599||||-41.287945|174.767166',
-      '76|137|OA|10.0|-41.2882585|174.7670996|L|-41.2882844|174.7672309',
-      '92|137|VERTEX|10.378||||-41.288304|174.767227',
-      '77|137|OA|11.0|-41.2880114|174.7674035|R|-41.2880839|174.7672213',
-      '95|137|VERTEX|11.773||||-41.288136|174.767242',
-      '78|137|OA|12.0|-41.2884049|174.7670334|L|-41.2884589|174.7671618',
-      '94|137|VERTEX|12.242||||-41.28817|174.767242',
-      '93|137|VERTEX|13.042||||-41.288228|174.767242',
-      '91|137|VERTEX|14.14||||-41.288487|174.76715',
-      '90|137|VERTEX|15.758||||-41.288567|174.76712',
-      '89|137|VERTEX|17.598||||-41.288659|174.767089',
-      '79|137|OA|18.0|-41.2887878|174.7668435|L|-41.288758|174.767089',
-      '88|137|VERTEX|18.602||||-41.288712|174.767089',
-      '87|137|VERTEX|19.473||||-41.288758|174.767089',
-      '81|137|OA|20.0|-41.2888927|174.7667798|L|-41.2887843|174.767098',
-      '82|137|OA|22.0|-41.2889696|174.7667528|L|-41.288845|174.7671186',
-      '83|137|OA|24.0|-41.2890752|174.7671263|L|-41.2889837|174.7672284',
-      '84|137|OA|26.0|-41.2891657|174.7671712|L|-41.2891548|174.7673099'
+      '5|1|OA|1.0|-41.2871999|174.766753|R|-41.2873014|174.7666946',
+      '25|1|VERTEX|2.373||||-41.287388|174.766845',
+      '24|1|VERTEX|3.207||||-41.287461|174.766921',
+      '23|1|VERTEX|3.959||||-41.287533|174.766983',
+      '22|1|VERTEX|4.637||||-41.287606|174.767028',
+      '10|1|OA|7.0|-41.2877481|174.7674712|R|-41.2878827|174.7671406',
+      '11|1|OA|9.0|-41.2878189|174.7673061|R|-41.2878858|174.7671419',
+      '21|1|VERTEX|9.599||||-41.287945|174.767166',
+      '1|1|OA|10.0|-41.2882585|174.7670996|L|-41.2882844|174.7672309',
+      '17|1|VERTEX|10.378||||-41.288304|174.767227',
+      '2|1|OA|11.0|-41.2880114|174.7674035|R|-41.2880839|174.7672213',
+      '20|1|VERTEX|11.773||||-41.288136|174.767242',
+      '3|1|OA|12.0|-41.2884049|174.7670334|L|-41.2884589|174.7671618',
+      '19|1|VERTEX|12.242||||-41.28817|174.767242',
+      '18|1|VERTEX|13.042||||-41.288228|174.767242',
+      '16|1|VERTEX|14.14||||-41.288487|174.76715',
+      '15|1|VERTEX|15.758||||-41.288567|174.76712',
+      '14|1|VERTEX|17.598||||-41.288659|174.767089',
+      '4|1|OA|18.0|-41.2887878|174.7668435|L|-41.288758|174.767089',
+      '13|1|VERTEX|18.602||||-41.288712|174.767089',
+      '12|1|VERTEX|19.473||||-41.288758|174.767089',
+      '6|1|OA|20.0|-41.2888927|174.7667798|L|-41.2887843|174.767098',
+      '7|1|OA|22.0|-41.2889696|174.7667528|L|-41.288845|174.7671186',
+      '8|1|OA|24.0|-41.2890752|174.7671263|L|-41.2889837|174.7672284',
+      '9|1|OA|26.0|-41.2891657|174.7671712|L|-41.2891548|174.7673099'
     ]);
 
     t.end();
@@ -126,12 +126,12 @@ module.exports.functional.end_to_end = function(test) {
 
 // write geojson to disk
 module.exports.functional.geojson = function(test) {
-  action.geojson(test, paths, 'id=137');
+  action.geojson(test, paths, 'id=1');
 };
 
 // write tsv to disk
 module.exports.functional.tsv = function(test) {
-  action.tsv(test, paths, 'id=137');
+  action.tsv(test, paths, 'id=1');
 };
 
 module.exports.all = function (tape) {
