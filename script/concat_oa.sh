7c641a308d1ffff7c64290fc9f13e5e9ec5bfcef
Peter Johnson <insomnia@rcpt.at>
2017-01-04 19:11:47 +0100

    update concat_oa script to add hashes and hash prefixes

--- script/concat_oa.sh
+++ script/concat_oa.sh
@@ -29,17 +29,27 @@ find $OAPATH -type f -iname "*.csv" ! -name '*summary*' -print0 |\
     # echo filename to stderr
     >&2 echo $(date -u) "$filename";
 
+    # generate the hash prefix for records in this file
+    HASH_PREFIX="${filename#$OAPATH/}";
+    HASH_PREFIX="${HASH_PREFIX%.*}";
+
     # start reading from line 2 (discard header)
     tail -n +2 $filename |\
 
       # remove newline characters inside quoted text
       awk -v RS='"' 'NR % 2 == 0 { gsub(/\r?\n|\r/, " ") } { printf("%s%s", $0, RT) }' |\
 
-        # sort the file by STREET, CITY, DISTRICT, REGION, NUMBER
-        sort -t, -k 4,4d -k 6,6d -k 7,7d -k 8,8d -k 3,3n |\
+        # create a HASH column for those which don't have one (or a very short one)
+        awk -F ',' 'BEGIN { OFS = "," } { if( length($NF) < 2 ) $NF = sprintf( "%d", NR ); print }' |\
 
-          # remove duplicates
-          uniq;
+          # prepend the file name to the HASH column
+          awk -F',' -v prefix=$HASH_PREFIX 'BEGIN { OFS = "," } { $NF = sprintf( "%s:%s", prefix, $NF ); print }' |\
+
+            # sort the file by STREET, CITY, DISTRICT, REGION, NUMBER
+            sort -t, -k 4,4d -k 6,6d -k 7,7d -k 8,8d -k 3,3n |\
+
+              # remove duplicates
+              uniq;
   done;
 
 # awk test case:
