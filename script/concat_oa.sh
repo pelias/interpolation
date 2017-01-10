a1d4be7bd93a2638189223b0e45bb7c00fc34039
Peter Johnson <insomnia@rcpt.at>
2017-01-10 15:04:30 +0100

    script: remove windows® newlines from oa files, fix parser bug

--- script/concat_oa.sh
+++ script/concat_oa.sh
@@ -23,7 +23,7 @@ find $OAPATH -type f -iname "*.csv" ! -name '*summary*' -print0 |\
     # LON,LAT,NUMBER,STREET,UNIT,CITY,DISTRICT,REGION,POSTCODE,ID,HASH
     if [ "$HAS_OUTPUT_HEADER" = false ] ; then
       HAS_OUTPUT_HEADER=true;
-      head -n1 $filename;
+      head -n1 $filename | sed $'s/\r//'; # remove windows® newlines
     fi
 
     # echo filename to stderr
@@ -36,20 +36,23 @@ find $OAPATH -type f -iname "*.csv" ! -name '*summary*' -print0 |\
     # start reading from line 2 (discard header)
     tail -n +2 $filename |\
 
-      # remove newline characters inside quoted text
-      awk -v RS='"' 'NR % 2 == 0 { gsub(/\r?\n|\r/, " ") } { printf("%s%s", $0, RT) }' |\
+      # remove windows® newlines
+      sed $'s/\r//' |\
 
-        # create a HASH column for those which don't have one (or a very short one)
-        awk -F ',' 'BEGIN { OFS = "," } { if( length($NF) < 2 ) $NF = sprintf( "%d", NR ); print }' |\
+        # remove newline characters inside quoted text
+        awk -v RS='"' 'NR % 2 == 0 { gsub(/\r?\n|\r/, " ") } { printf("%s%s", $0, RT) }' |\
 
-          # prepend the file name to the HASH column
-          awk -F',' -v prefix=$HASH_PREFIX 'BEGIN { OFS = "," } { $NF = sprintf( "%s:%s", prefix, $NF ); print }' |\
+          # create a HASH column for those which don't have one (or a very short one)
+          awk -F ',' 'BEGIN { OFS = "," } { if( length($NF) < 2 ) $NF = sprintf( "%d", NR ); print }' |\
 
-            # sort the file by STREET, CITY, DISTRICT, REGION, NUMBER
-            sort -t, -k 4,4d -k 6,6d -k 7,7d -k 8,8d -k 3,3n |\
+            # prepend the file name to the HASH column
+            awk -F',' -v prefix=$HASH_PREFIX 'BEGIN { OFS = "," } { $NF = sprintf( "%s:%s", prefix, $NF ); print }' |\
 
-              # remove duplicates
-              uniq;
+              # sort the file by STREET, CITY, DISTRICT, REGION, NUMBER
+              sort -t, -k 4,4d -k 6,6d -k 7,7d -k 8,8d -k 3,3n |\
+
+                # remove duplicates
+                uniq;
   done;
 
 # awk test case:
