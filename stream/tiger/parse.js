83585d50568d69f12f0de0fb116d62e5733d2bd5
Peter Johnson <insomnia@rcpt.at>
2017-01-19 15:50:15 +0100

    tiger: sort feature collection for performant batching

--- stream/tiger/parse.js
+++ stream/tiger/parse.js
@@ -1,14 +1,16 @@
 
 var through = require('through2');
 
+// see: http://www2.census.gov/geo/pdfs/maps-data/data/tiger/tgrshp2016/TGRSHP2016_TechDoc_Ch4.pdf
+
 function streamFactory(){
 
-  var buffer = [];
+  var buffer = '';
 
   return through.obj(function( chunk, _, next ){
 
     // buffer the whole file
-    buffer.push( chunk.toString('utf8') );
+    buffer += chunk.toString('utf8');
     next();
 
   }, function flush( done ){
@@ -16,7 +18,30 @@ function streamFactory(){
     try {
 
       // parse json
-      var collection = JSON.parse( buffer.join('') );
+      var collection = JSON.parse( buffer );
+
+      // sort features so that ranges on the same edge group next to each other
+      // note: this increases performance by reducing by batching street lookups
+      collection.features.sort( function( a, b ){
+
+        // The permanent edge identifier (TLID) attribute
+        var edgeA = parseInt( a.properties.TLID, 10 );
+        var edgeB = parseInt( b.properties.TLID, 10 );
+
+        // The full street name (eg. "FULLNAME": "Paladino Ave")
+        var nameA = a.properties.FULLNAME;
+        var nameB = b.properties.FULLNAME;
+
+        // Sort on TLID first
+        if(edgeA < edgeB){ return -1; }
+        if(edgeA > edgeB){ return 1; }
+
+        // Otherwise sort on FULLNAME
+        if(nameA < nameB){ return -1; }
+        if(nameA > nameB){ return 1; }
+
+        return 0;
+      });
 
       // push each feature downstream
       collection.features.forEach( function( feat ){
