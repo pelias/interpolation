56daa1577b66d1ab775eba7f3dc0d032b9d4673e
Julian Simioni <julian@mapzen.com>
2017-03-13 16:52:43 -0400

    Base PBF2JSON_BIN path on local copy

--- script/build.sh
+++ script/build.sh
@@ -12,7 +12,7 @@ export LC_ALL=en_US.UTF-8;
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 
 export TIMESTAMP=$(date +"%m-%d-%Y-%H:%M:%S");
-export PBF2JSON_BIN="$DIR/node_modules/pbf2json/build/pbf2json.linux-x64";
+export PBF2JSON_BIN="$DIR/../node_modules/pbf2json/build/pbf2json.linux-x64";
 
 # location of data files
 export POLYLINE_FILE="/data/polyline/planet.polylines"; # the file containing all the streets
