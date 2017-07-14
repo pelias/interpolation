910458017a85596672ec05d3814d3f52eb0fc772
Diana Shkolnikov <dianashk@gmail.com>
2017-07-13 20:35:11 -0400

    add logging, only set vars if not defined

--- script/build.sh
+++ script/build.sh
@@ -15,10 +15,13 @@ export TIMESTAMP=$(date +"%m-%d-%Y-%H:%M:%S");
 export PBF2JSON_BIN="$DIR/../node_modules/pbf2json/build/pbf2json.linux-x64";
 
 # location of data files
-export WORKINGDIR="${WORKINGDIR:-"/mnt/pelias"}"
-export POLYLINE_FILE="$WORKINGDIR/data/polylines/planet.polylines"; # the file containing all the streets
-export OAPATH="$WORKINGDIR/data/oa"; # base path of openaddresses file system
-export PBF2JSON_FILE="$WORKINGDIR/data/osm/planet-latest.osm.pbf";
+export WORKINGDIR="${WORKINGDIR:-"/mnt/pelias"}";
+# the file containing all the streets
+export POLYLINE_FILE="${POLYLINE_FILE:-"$WORKINGDIR/data/polylines/planet.polylines"}";
+# base path of openaddresses file system
+export OAPATH="${OAPATH:-"$WORKINGDIR/data/oa"}";
+ # base path of osm pbf file
+export PBF2JSON_FILE="${PBF2JSON_FILE:-"$WORKINGDIR/data/osm/planet-latest.osm.pbf"}";
 
 # a directory where all builds will live
 BUILDS="$WORKINGDIR/interpolation/builds";
@@ -27,35 +30,46 @@ BUILDS="$WORKINGDIR/interpolation/builds";
 [ -d $BUILDS ] || mkdir -p $BUILDS;
 
 # a directory where this specific build will live
-export BUILDDIR="$BUILDS/$TIMESTAMP";
+export BUILDDIR="${BUILDDIR:-"$BUILDS/$TIMESTAMP"}";
 [ -d $BUILDDIR ] || mkdir -p $BUILDDIR;
 
 # location of temp files
 export SQLITE_TMPDIR="$BUILDDIR/tmp"; # a directory with enough free space to store sqlite tmp files
 export PBF2JSON_TMPDIR="$BUILDDIR/tmp/leveldb"; # a directory with enough free space to store leveldb tmp files
 
+echo "$BUILDDIR"
+echo "$WORKINGDIR"
+
 # run polyline importer
+echo "- importing polylines"
 $DIR/import.sh;
 
 # archive street database (using parallel gzip when available)
+echo "- archiving street database
 if type pigz >/dev/null
   then pigz -k -c --best "$BUILDDIR/street.db" > "$BUILDDIR/street.db.gz";
   else gzip -c --best "$BUILDDIR/street.db" > "$BUILDDIR/street.db.gz";
 fi
 
 # run openaddresses conflation
+echo "- conflating openaddresses"
 $DIR/conflate_oa.sh;
 
 # run openstreetmap conflation
+echo "- conflating openstreetmap"
 $DIR/conflate_osm.sh;
 
 # run tiger conflation
+echo "- conflating tiger"
 $DIR/conflate_tiger.sh;
 
 # run vertex interpolation
+echo "- interpolating vertices"
 $DIR/vertices.sh;
 
 # archive address database (using parallel gzip when available)
+echo "- archiving address database"
+
 if type pigz >/dev/null
   then pigz -k -c --best "$BUILDDIR/address.db" > "$BUILDDIR/address.db.gz";
   else gzip -c --best "$BUILDDIR/address.db" > "$BUILDDIR/address.db.gz";
@@ -66,6 +80,8 @@ rm -rf "$SQLITE_TMPDIR" "$PBF2JSON_TMPDIR"; # remove tmp files
 
 # record build meta data
 METAFILE="$BUILDDIR/build.meta";
+
+echo "- generating meta file"
 
 echo "-- file system --" > "$METAFILE";
 ls -lah "$BUILDDIR" >> "$METAFILE";
@@ -83,3 +99,7 @@ sqlite3 -echo "$BUILDDIR/address.db" "SELECT COUNT(*) FROM address;" >> "$METAFI
 
 # update 'current' symlink
 ln -sfn "$BUILDDIR" "$BUILDS/current";
+
+echo
+echo "Build completed!"
+echo
