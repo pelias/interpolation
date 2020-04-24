e81c3455296708ea8166cd1681739ac7db6d4bf6
missinglink <insomnia@rcpt.at>
2020-04-24 15:21:16 +0200

    refactor(pragma): set journal_mode=MEMORY for performance

--- query/configure.js
+++ query/configure.js
@@ -6,6 +6,6 @@ module.exports = ( db ) => {
   db.exec('PRAGMA main.page_size=4096;'); // (default: 1024)
   db.exec('PRAGMA main.cache_size=-2000;'); // (default: -2000, 2GB)
   db.exec('PRAGMA main.synchronous=OFF;');
-  db.exec('PRAGMA main.journal_mode=OFF;');
+  db.exec('PRAGMA main.journal_mode=MEMORY;'); // better-sqlite3 does not support 'OFF'
   db.exec('PRAGMA main.temp_store=MEMORY;');
 };
